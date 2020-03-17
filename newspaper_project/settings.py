c79986d1903ce6e94c72a3984e0ba3b111a3c360
LilPumpIsGod <alosha.alosh@ukr.net>
2020-03-17 23:56:29 +0200

    sign up/in/out completed

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'newspaper_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- users/forms.py
+++ users/forms.py
@@ -8,10 +8,10 @@ class CustomUserCreationForm(UserCreationForm):
 	
 	class Meta(UserChangeForm.Meta):
 		model = CustomUser
-		fields = UserCreationForm.Meta.fields + ('age', )
+		fields = ('username', 'email', 'age', )
 
 class CustomUserChangeForm(UserChangeForm):
 
 	class Meta:
 		model = CustomUser
-		fields = UserChangeForm.Meta.fields
+		fields = ('username', 'email', 'age', )
